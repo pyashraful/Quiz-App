357e8888d2459e3d4d0680212d883ec41cec8762
Py-Ashraful <ashrafulislam180@gmail.com>
2021-09-16 22:22:01 +0600

    miniPlayer view

--- src/components/MiniPlayer.js
+++ src/components/MiniPlayer.js
@@ -1,15 +1,35 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import miniPlayerImage from "../assets/images/3.jpg";
 import classes from "../styles/MiniPlayer.module.css";
 
 export const MiniPlayer = () => {
+  const [status, setStatus] = useState(false);
+  const buttonRef = useRef();
+
+  function toggleMiniPlayer() {
+    if (!status) {
+      buttonRef.current.classList.remove(classes.floatingBtn);
+      setStatus(true);
+    } else {
+      buttonRef.current.classList.add(classes.floatingBtn);
+      setStatus(false);
+    }
+  }
+
   return (
-    <div className={`${classes.miniPlayer} ${classes.floatingBtn}`}>
+    <div
+      ref={buttonRef}
+      onClick={toggleMiniPlayer}
+      className={`${classes.miniPlayer} ${classes.floatingBtn}`}
+    >
       <span className={`material-icons-outlined ${classes.open}`}>
         {" "}
         play_circle_filled{" "}
       </span>
-      <span className={`material-icons-outlined ${classes.close}`}>
+      <span
+        className={`material-icons-outlined ${classes.close}`}
+        onClick={toggleMiniPlayer}
+      >
         {" "}
         close{" "}
       </span>
